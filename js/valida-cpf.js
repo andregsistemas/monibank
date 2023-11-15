b28a2c90fad077d943bb4d29ab53c92d89bf43dc
dukeossuna <andregsistemas@outlook.com>
2023-11-15 13:00:07 -0300

    Arquivos - 05.Capturando fotos

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -2,10 +2,8 @@ export default function ehUmCPF(campo){
     const cpf = campo.value.replace(/\.|-/g, "");
 
     if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
-        console.log("Esse CPF não existe!");
-    } else {
-        console.log("Esse CPF existe!");
-    }
+        campo.setCustomValidity('Esse CPF não é Valido');
+    } 
 }
 
 function validaNumerosRepetidos(cpf) {
